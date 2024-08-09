311b55842eabc969d80acef832f4f2b2ce37aac6
Lorenzo Gandino <lorenzoga94@gmail.com>
2024-08-09 23:52:40 +0200

    mock

--- src/pages/identity-attribute-page/identity-attribute.spec.tsx
+++ src/pages/identity-attribute-page/identity-attribute.spec.tsx
@@ -2,6 +2,9 @@ import { render } from '@testing-library/react';
 
 import IdentityAttribute from './identity-attribute';
 
+vi.mock("../../lib/auth", () => ({}));
+vi.mock("react-router-dom");
+
 describe('IdentityAttribute', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<IdentityAttribute />);
