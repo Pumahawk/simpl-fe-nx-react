1a30958b124289c6a7ce2d6d12efc4a8d13f0b8a
Lorenzo Gandino <lorenzoga94@gmail.com>
2024-08-13 18:06:32 +0200

    implementation filter logic, create Text filter

--- src/lib/component/filters-component/filters.spec.tsx
+++ src/lib/component/filters-component/filters.spec.tsx
@@ -1,10 +1,38 @@
-import { render } from '@testing-library/react';
+import { fireEvent, getByText, render, screen } from '@testing-library/react';
+import { TextFilter } from './filters';
 
-import Filters from './filters';
-
-describe('Filters', () => {
+describe('TextFilter', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Filters />);
+    const tf = new TextFilter({
+      label: "label",
+      name: "input",
+    });
+    const { baseElement } = render(tf.render());
     expect(baseElement).toBeTruthy();
   });
+  it('should change value in object', () => {
+    const tf = new TextFilter({
+      label: "label",
+      name: "input",
+    });
+    render(tf.render());
+
+    const input = screen.getByTestId('input');
+    fireEvent.change(input, {
+      target: {
+        value: 'Test update form'
+      }
+    });
+
+    expect(tf.getValue()).toBe('Test update form');
+  })
+  it('should render correct label', () => {
+    const ts = new TextFilter({
+      name: "Input name",
+      label: "Test label",
+    })
+    render(ts.render());
+
+    getByText(screen.getByTestId('label'), 'Test label');
+  })
 });
--- src/lib/component/filters-component/filters.tsx
+++ src/lib/component/filters-component/filters.tsx
@@ -1,7 +1,48 @@
-export function Filters() {
-  return (
-      <>working...</>
-  )
+import { MutableRefObject, ReactNode } from "react";
+
+export interface Filter<T> {
+  getValue(): T;
+  render(): ReactNode;
 }
 
-export default Filters;
+interface TextFilterConfig {
+  name: string,
+  label: string,
+}
+export class TextFilter implements Filter<string> {
+  _name: string;
+  _label: string;
+  _input: MutableRefObject<HTMLInputElement | null> = {current: null};
+
+  constructor({name, label}: TextFilterConfig) {
+    this._name = name;
+    this._label = label;
+  }
+
+  getValue(): string {
+    if (this._input.current?.value == null) {
+      throw new Error("Invalid implementation of TextFilter");
+    } else {
+      return this._input.current.value;
+    }
+  }
+
+  render(): ReactNode {
+    return <TextFilterComponent name={this._name} label={this._label} inputRef={this._input}/>;
+  }
+}
+
+interface TextFilterComponentProp {
+  name: string;
+  label: string;
+  inputRef: MutableRefObject<HTMLInputElement | null>;
+
+}
+function TextFilterComponent({name, label, inputRef}: TextFilterComponentProp) {
+  return (
+    <div>
+      <label data-testid="label">{label}</label>
+      <input name={name} data-testid="input" ref={inputRef} type="text"/>
+    </div>
+  );
+}
