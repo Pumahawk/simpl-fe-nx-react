45090ecb1a4c91cd6f64061af6f22a8041e11426
Lorenzo Gandino <lorenzoga94@gmail.com>
2024-08-13 18:45:43 +0200

    generated filter bar component

---
+++ src/lib/component/filter-bar-component/filter-bar.tsx
@@ -0,0 +1,11 @@
+import styles from './filter-bar.module.scss';
+
+export function FilterBar() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to FilterBar!</h1>
+    </div>
+  );
+}
+
+export default FilterBar;
