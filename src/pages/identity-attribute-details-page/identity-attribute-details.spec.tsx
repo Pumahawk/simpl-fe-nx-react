f2036e9e90ffe701248538515d45e83dd2c4e4a1
Lorenzo Gandino <lorenzoga94@gmail.com>
2024-08-13 18:19:44 +0200

    fix test identity-attribute-details

--- src/pages/identity-attribute-details-page/identity-attribute-details.spec.tsx
+++ src/pages/identity-attribute-details-page/identity-attribute-details.spec.tsx
@@ -2,8 +2,15 @@ import { render } from '@testing-library/react';
 
 import IdentityAttributeDetails from './identity-attribute-details';
 
+import { useLoaderData } from 'react-router-dom';
+
+vi.mock('react-router-dom');
+
 describe('IdentityAttributeDetails', () => {
   it('should render successfully', () => {
+    vi.mocked(useLoaderData).mockReturnValue({
+      deferred: {}
+    })
     const { baseElement } = render(<IdentityAttributeDetails />);
     expect(baseElement).toBeTruthy();
   });
