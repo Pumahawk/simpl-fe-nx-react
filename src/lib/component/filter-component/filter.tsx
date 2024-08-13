7320756924955da322d19f160dbe5706b872946a
Lorenzo Gandino <lorenzoga94@gmail.com>
2024-08-13 17:25:25 +0200

    generate filter component

---
+++ src/lib/component/filter-component/filter.tsx
@@ -0,0 +1,11 @@
+import styles from './filter.module.scss';
+
+export function Filter() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Filter!</h1>
+    </div>
+  );
+}
+
+export default Filter;
