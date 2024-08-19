a7175be733b59524a1edb1c3bf48b05ee8ec4f14
Lorenzo Gandino <lorenzoga94@gmail.com>
2024-08-19 23:29:05 +0200

    removed boostrap

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from "./router";
-import "bootstrap/dist/js/bootstrap.js";
 import { LoadingPage } from './lib/component/loading-component/loading';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#161925",
+      "primary-light": "#23395b",
+      secondary: "#8ea8c3",
+      "secondary-light": "#cbf7ed",
+    },
     extend: {},
   },
   plugins: [],
