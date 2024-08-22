cb170d80cdbae2eb0f1eb60c74f1145ad66c78a3
Lorenzo Gandino <lorenzoga94@gmail.com>
2024-08-22 20:57:32 +0200

    create selection input

--- src/lib/component/form-inputs-component/form-inputs.tsx
+++ src/lib/component/form-inputs-component/form-inputs.tsx
@@ -12,7 +12,29 @@ export function Input({label, id, name, input, type="text"}: InputProps) {
   return (
     <div className='relative mt-5'>
       <input className='peer w-full border outline-0 pt-3 pb-4 px-3 border rounded-md focus:outline-1 focus:outline-primary-800 ' type={type} id={id} name={name} {...input}></input>
-      <label className='absolute -top-2.5 left-2 bg-white px-3 text-xs text-slate-500 peer-focus:text-primary-800 peer-focus:font-semibold' htmlFor={id}>{label}</label>
+      <label className='absolute -top-2.5 left-2 bg-white px-3 text-xs rounded-md text-slate-500 peer-focus:text-primary-800 peer-focus:font-semibold' htmlFor={id}>{label}</label>
+    </div>
+  );
+}
+
+export interface SelectProps {
+  id: string;
+  label: string;
+  name: string;
+  options: {
+    label: string;
+    id: string;
+    value: string;
+  }[],
+  select?: HTMLProps<HTMLSelectElement>
+}
+export function Select({id, label, select, options}: SelectProps) {
+  return (
+    <div className='relative mt-5'>
+    <select {...select} className='peer w-full border outline-0 pt-3 pb-4 px-3 border rounded-md focus:outline-1 focus:outline-primary-800'>
+      { options.map(option => <option key={option.id} value={option.value}>{option.label}</option>) }
+    </select>
+    <label className='absolute -top-2.5 left-2 bg-white px-3 text-xs rounded-md text-slate-500 peer-focus:text-primary-800 peer-focus:font-semibold' htmlFor={id}>{label}</label>
     </div>
   );
 }
--- src/pages/application-request-page/application-request.tsx
+++ src/pages/application-request-page/application-request.tsx
@@ -1,11 +1,31 @@
-import { Input } from '../../lib/component/form-inputs-component/form-inputs';
+import { Input, Select } from '../../lib/component/form-inputs-component/form-inputs';
 import { CenterPageLayout } from '../../lib/component/page-layout-component/page-layout';
 
 export function ApplicationRequest() {
+  const participantTypeOptions = [
+    {
+      label: "Consumer",
+      value: "CONSUMER",
+      id: "CONSUMER",
+    },
+    {
+      label: "Service provider",
+      value: "SERVICE_PROVIDER",
+      id: "SERVICE_PROVIDER",
+    }
+  ]
   return (
     <CenterPageLayout title='Welcome to $Dataspace'>
-      <form>
+      <form className='bg-red-300'>
         <Input id='email' label='Email address' name='email'/>
+        <Input id='org' label='Organizzation name' name='org'/>
+        <Select id='participantType' label='Participant type' name='participantType' options={participantTypeOptions}/>
+        <Input id='surname' label='Surname' name='surname'/>
+        <Input id='name' label='Name' name='name'/>
+        <Input id='username' label='Username' name='username'/>
+        <Input id='password' label='Password' name='password'/>
+        <Input id='confirmPassword' label='Confirm Password' name='confirmPassword'/>
+        <button className='mt-6 mb-3 btn btn-primary' type="button">Create credentials</button>
       </form>
     </CenterPageLayout>
   );
